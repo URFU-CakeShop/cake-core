65f45f5aed89860cdd3bcc831f9a94d7137f3a1a
Artamonov Vyacheslav <igrok9636@gmail.com>
2026-03-05 20:56:08 +0500

    Initial commit

---
+++ src/main/java/ru/urfu/cake/CakeCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.cake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeCoreApplication.class, args);
+	}
+
+}
